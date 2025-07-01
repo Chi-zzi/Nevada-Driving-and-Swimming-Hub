a5d5ab69691724669dd04ab1ec98b53ff231a8fc
Chidubem <chukwudichukwudubem1@gmail.com>
2025-07-01 18:09:47 +0100

    first commit

--- Nevada/settings.py
+++ Nevada/settings.py
@@ -148,7 +148,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 
